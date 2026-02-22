987556ff2273f3157ffff4bc14b530c1c79a191e
Bogdan <>
2026-02-22 11:02:02 +0200

    wait mechanism works different for all list-does not wait,like for first or nth element

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -10,6 +10,7 @@ test.only('Browser Context Playwright test', async ({browser})=>
     console.log(await page.title());
     const userName = page.locator('#username');
     const signIn = page.locator('#signInBtn');
+    const cardTitles = page.locator('.card-body a');
     await userName.fill("rahulshetty");
     await page.locator('[type="password"]').fill("Learning@830$3mK2");
     await signIn.click();
@@ -17,8 +18,10 @@ test.only('Browser Context Playwright test', async ({browser})=>
     await expect(page.locator('[style*="block"]')).toContainText("Incorrect");
     await userName.fill("rahulshettyacademy");
     await signIn.click();
-    console.log(await page.locator('.card-body a').first().textContent());
-    console.log(await page.locator('.card-body a').nth(1).textContent());
+    console.log(await cardTitles.first().textContent());
+    console.log(await cardTitles.nth(1).textContent());
+    const allTitles = await cardTitles.allTextContents();
+    console.log(allTitles);
     
 
     //await page.pause();
