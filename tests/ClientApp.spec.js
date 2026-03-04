39fe60db15708a14dc7b85422186b82e73255b88
Bogdan <>
2026-03-04 07:58:57 +0200

    select from dropdown with press sequentially and finished te order

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -5,9 +5,10 @@ test.only('Browser Context Playwright test', async ({page})=>
 {
     const productName = "ZARA COAT 3";
     const products = page.locator(".card-body");
+    const email = "anshika@gmail.com";
 
     await page.goto("https://rahulshettyacademy.com/client");
-    await page.locator("#userEmail").fill("anshika@gmail.com");
+    await page.locator("#userEmail").fill(email);
     await page.locator("#userPassword").fill("Iamking@000");
     await page.locator("[value='Login']").click();
     //await page.waitForLoadState('networkidle');  can be flaky, better to wait for specific element
@@ -31,6 +32,26 @@ test.only('Browser Context Playwright test', async ({page})=>
     await page.locator("div li").first().waitFor();
     const bool =await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
     expect(bool).toBeTruthy();
+    await page.locator("text=Checkout").click();
+    //delay means that it presses "i" waits 150 miliseconds and then presses "n" wait 150 milisecnods...
+    await page.locator("[placeholder*='Country']").pressSequentially("ind", {delay:150});
+    const dropdown = page.locator(".ta-results");
+    await dropdown.waitFor();
+    const optionsCount = await dropdown.locator("button").count();
+    for(let i=0; i<optionsCount; ++i){
+        const text = await dropdown.locator("button").nth(i).textContent();
+        if(text === " India"){
+            await dropdown.locator("button").nth(i).click();
+            break;
+        }
+    }
+
+    await expect(page.locator(".user__name [type=text]").first()).toHaveText(email);
+    await page.locator(".action__submit").click();
+    expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+    const orderID = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+    console.log(orderID);
+
 
     await page.pause();
 });
