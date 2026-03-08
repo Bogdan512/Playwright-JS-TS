75094664f6e904f83e56180568173c23133e2fea
Bogdan <>
2026-03-08 19:39:52 +0200

    for loop for expected date calendar to match with what was given

--- tests/Calendar.spec.js
+++ tests/Calendar.spec.js
@@ -6,6 +6,8 @@ test("Calendar validation", async ({page})=>
     const date = "15";
     const year = "2027";
 
+    const expectedList = [monthNumber, date, year];
+
     await page.goto("https://www.rahulshettyacademy.com/SeleniumPractise/#/offers");
     await page.locator("div.react-date-picker__inputGroup").click();
     await page.locator(".react-calendar__navigation__label").click();
@@ -13,5 +15,13 @@ test("Calendar validation", async ({page})=>
     await page.getByText(year).click();
     await page.locator(".react-calendar__year-view__months__month").nth(Number(monthNumber-1)).click();
     await page.locator("//abbr[text()='" + date + "']").click();
+
+    const input = page.locator("div .react-date-picker__inputGroup__input");
+
+    for(let i=0; i<expectedList.length; ++i)
+    {
+        const value = await input.nth(i).inputValue();
+        expect(value).toEqual(expectedList[i]);
+    }
 
 });
