ef618fef4365bf348d9981748937971dfe9c2dce
Bogdan <ciurtinbogdan@yahoo.com>
2026-04-19 18:12:54 +0300

    adding extra config file

---
+++ playwright.config1.js
@@ -0,0 +1,42 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+const config =({
+  testDir: './tests',
+  // 40 seconds for each test
+  timeout: 40 * 1000,
+// Expect a condition to  be met within 30 seconds
+  expect: {
+    timeout: 5000
+  },
+  reporter: 'html',
+  projects : 
+  [
+    {
+      name : 'safari',
+      use: 
+      {
+        browserName: 'webkit',
+        headless: true,
+        screenshot: 'on',
+        //trace: 'on',
+      }
+    },
+    {
+      name : 'chrome',
+      use: 
+      {
+        browserName: 'chromium',
+        headless: false,
+        screenshot: 'on',
+        //trace: 'on',
+      }
+    }
+  ]
+});
+
+module.exports = config;
+
